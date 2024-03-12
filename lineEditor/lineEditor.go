7478d3769bf5b9597d817c691c6b2b68b00d394d
Marcos Brito <marcos.brito0507@gmail.com>
2024-03-11 22:52:38 -0300

    refactor(lineEditor): move api functions to a file

--- lineEditor/lineEditor.go
+++ lineEditor/lineEditor.go
@@ -1,9 +1,7 @@
 package lineEditor
 
 import (
-	"fmt"
 	"math"
-	"os"
 
 	"github.com/marcos-brito/arosh/lineEditor/event"
 	curses "github.com/rthornton128/goncurses"
@@ -175,105 +173,4 @@ func (editor *LineEditor) positionToCoordinate(position int) (y int, x int) {
 	}
 
 	return row, column
-}
-
-// API
-
-func Put(editor *LineEditor, str string) {
-	// HACK: The current implementation for the `Text` (a piece table) has some flaws.
-	// If the original buffer is empty then both the first and the second insertion have to be done
-	// at index 0.
-	if editor.position == 0 || editor.position == 1 {
-		editor.add(str, 0)
-		editor.position += len(str)
-		return
-	}
-
-	editor.add(str, editor.position-1)
-	editor.position += len(str)
-	editor.eventManager.Notify(event.TEXT_PUTTED)
-}
-
-func GetLineContent(editor *LineEditor) string {
-	return editor.text.text()
-}
-
-func SetLineContent(editor *LineEditor, str string) {
-	editor.deleteAll()
-	editor.add(str, 0)
-}
-
-func DeleteBehind(editor *LineEditor) {
-	if editor.position == 0 {
-		return
-	}
-
-	editor.delete(editor.position - 1)
-	editor.position--
-}
-
-func DeleteAll(editor *LineEditor) {
-	editor.deleteAll()
-}
-
-func Print(editor *LineEditor, text string) {
-	editor.print(text)
-}
-
-func Error(editor *LineEditor, err string) {
-	editor.print(fmt.Sprintf("error: %s", err))
-}
-
-func AddWidget(editor *LineEditor, widget Widget) {
-	editor.widgets = append(editor.widgets, widget)
-}
-
-func On(editor *LineEditor, event event.Event, listener event.Listener) {
-	editor.eventManager.AddListener(event, listener)
-}
-
-func NewBinding(editor *LineEditor, key curses.Key, command func(*LineEditor)) error {
-	err := newBinding(key, command)
-
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func OverwriteBiding(editor *LineEditor, key curses.Key, command func(*LineEditor)) {
-	overwriteBiding(key, command)
-}
-
-func MoveToN(editor *LineEditor, n int) {
-	editor.moveToN(n)
-}
-
-func MoveLeft(editor *LineEditor) {
-	editor.moveToN(editor.position - 1)
-}
-
-func MoveRight(editor *LineEditor) {
-	editor.moveToN(editor.position + 1)
-}
-
-func StartOfLine(editor *LineEditor) {
-	editor.moveToN(0)
-}
-
-func EndOfLine(editor *LineEditor) {
-	editor.moveToN(len(editor.text.text()))
-}
-
-func AcceptLine(editor *LineEditor) {
-	editor.print(fmt.Sprintf("%s%s", editor.prompt, editor.text.text()))
-	editor.eventManager.Notify(event.LINE_ACCEPTED)
-
-	editor.deleteAll()
-}
-
-func Exit(editor *LineEditor) {
-	curses.End()
-	os.Exit(0)
 }
