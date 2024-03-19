f179877737b57ff985941a28d7bddc0265bac97b
Marcos Brito <marcos.brito0507@gmail.com>
2024-03-19 09:18:43 -0300

    attach the history to the editor

--- bin/arosh/main.go
+++ bin/arosh/main.go
@@ -3,13 +3,20 @@ package main
 import (
 	"fmt"
 	"os"
+	"path"
 
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/marcos-brito/arosh/lineEditor"
+	"github.com/marcos-brito/arosh/widgets/history"
 )
 
 func main() {
+	home, _ := os.LookupEnv("HOME")
+
 	editor := lineEditor.NewLineEditor()
+	history := history.NewHistory(path.Join(home, ".arosh_history"))
+	lineEditor.AddWidget(editor, history)
+
 	p := tea.NewProgram(editor)
 
 	if _, err := p.Run(); err != nil {
