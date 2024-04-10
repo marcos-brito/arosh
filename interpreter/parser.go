38d560894c4877db160bb8f49964a0e7249a03dd
Marcos Brito <marcos.brito0507@gmail.com>
2024-04-10 12:40:35 -0300

    feat(interpreter): create parsing rules until compound_command

--- interpreter/parser.go
+++ interpreter/parser.go
@@ -1,24 +1,112 @@
 package interpreter
 
 type Parser struct {
-	lexer *Lexer
+	lexer   *Lexer
+	current Token
 }
 
-func ParserNew(lexer *Lexer) *Parser {
+func NewParser(lexer *Lexer) *Parser {
 	return &Parser{
-		lexer: lexer,
+		lexer:   lexer,
+		current: lexer.NextToken(),
 	}
 }
 
-func (p *Parser) Parse() {
-	p.program()
-}
-
-func (p *Parser) program() {
-	for p.lexer.NextToken().t != EOF {
-		p.sequence()
+func (p *Parser) match(types ...tokenType) bool {
+	for _, t := range types {
+		if p.current.t == t {
+			return true
+		}
 	}
+
+	return false
 }
 
-func (p *Parser) sequence() {
+func (p *Parser) next() {
+	p.current = p.lexer.NextToken()
+}
+
+func (p *Parser) Parse() *Program {
+	return p.program()
+}
+
+func (p *Parser) program() *Program {
+	program := &Program{}
+
+	for !p.match(EOF) {
+		program.nodes = append(program.nodes, p.sequence())
+	}
+
+	return program
+}
+
+func (p *Parser) sequence() Node {
+	lhs := p.conditional()
+
+	for p.match(SEMI, AND) {
+		separator := p.current.lexeme
+		p.next()
+		rhs := p.conditional()
+		lhs = &Sequence{separator: separator, lhs: lhs, rhs: rhs}
+	}
+
+	return lhs
+}
+
+func (p *Parser) conditional() Node {
+	lhs := p.pipe()
+
+	for p.match(DAND, DPIPE) {
+		conditionalType := p.current.lexeme
+		p.next()
+		rhs := p.pipe()
+		lhs = &Conditional{conditionalType: conditionalType, lhs: lhs, rhs: rhs}
+	}
+
+	return lhs
+}
+
+func (p *Parser) pipe() Node {
+	lhs := p.command()
+
+	for p.match(PIPE) {
+		p.next()
+		rhs := p.command()
+		lhs = &Pipe{lhs: lhs, rhs: rhs}
+	}
+
+	return lhs
+}
+
+func (p *Parser) command() Node {
+	if p.match(FUNCTION) {
+		return p.function()
+	}
+
+	if p.match(NAME) {
+		return p.simpleCommand()
+	}
+
+	return p.compoundCommand()
+}
+
+func (p *Parser) function() Node {
+	return nil
+}
+
+func (p *Parser) simpleCommand() Node {
+	name := p.current.lexeme
+	params := []string{}
+
+	p.next()
+	for p.match(NAME, WORD) {
+		params = append(params, p.current.lexeme)
+		p.next()
+	}
+
+	return &SimpleCommand{name: name, params: params}
+}
+
+func (p *Parser) compoundCommand() Node {
+	return nil
 }
---
+++ interpreter/parser_test.go
@@ -0,0 +1,122 @@
+package interpreter
+
+import (
+	"testing"
+)
+
+func TestParseSequence(t *testing.T) {
+	tests := []struct {
+		source   string
+		expected *Program
+	}{
+		{
+			"cat file | grep struct & echo sup | grep",
+			&Program{
+				nodes: []Node{
+					&Sequence{
+						separator: "&",
+						lhs: &Pipe{
+							lhs: &SimpleCommand{
+								name:   "cat",
+								params: []string{"file"},
+							},
+							rhs: &SimpleCommand{
+								name:   "grep",
+								params: []string{"struct"},
+							},
+						},
+						rhs: &Pipe{
+							lhs: &SimpleCommand{
+								name:   "echo",
+								params: []string{"sup"},
+							},
+							rhs: &SimpleCommand{
+								name: "grep",
+							},
+						},
+					},
+				},
+			},
+		},
+		{
+			"echo 123 & ls & pacman -Syu; nvim",
+			&Program{
+				nodes: []Node{
+					&Sequence{
+						separator: ";",
+						lhs: &Sequence{
+							separator: "&",
+							lhs: &Sequence{
+								separator: "&",
+								lhs: &SimpleCommand{
+									name:   "echo",
+									params: []string{"123"},
+								},
+								rhs: &SimpleCommand{
+									name: "ls",
+								},
+							},
+							rhs: &SimpleCommand{
+								name:   "pacman",
+								params: []string{"-Syu"},
+							},
+						},
+						rhs: &SimpleCommand{
+							name: "nvim",
+						},
+					},
+				},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		lexer := NewLexer(tt.source)
+		parser := NewParser(lexer)
+		got := parser.Parse()
+
+		if got.String() != tt.expected.String() {
+			t.Errorf(
+				"Got mismatching ast's for \"%s\": \n got: %s \n expected: %s",
+				tt.source,
+				got,
+				tt.expected,
+			)
+		}
+	}
+}
+
+func TestParseSimpleCommand(t *testing.T) {
+	tests := []struct {
+		source   string
+		expected *Program
+	}{
+		{
+			"tmux attach -t dotfiles",
+			&Program{
+				nodes: []Node{
+					&SimpleCommand{
+						name:   "tmux",
+						params: []string{"attach", "-t", "dotfiles"},
+					},
+				},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		lexer := NewLexer(tt.source)
+		parser := NewParser(lexer)
+		got := parser.Parse()
+
+		if got.String() != tt.expected.String() {
+			t.Errorf(
+				"Got mismatching ast's for \"%s\": \n got: %s \n expected: %s",
+				tt.source,
+				got,
+				tt.expected,
+			)
+		}
+	}
+
+}
