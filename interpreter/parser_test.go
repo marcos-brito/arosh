430208416d2cecdb1c34b963e2db8253882a6eb4
Marcos Brito <marcos.brito0507@gmail.com>
2024-04-16 21:52:01 -0300

    feat(interpreter): create test for conditionals

--- interpreter/parser_test.go
+++ interpreter/parser_test.go
@@ -130,6 +130,96 @@ func TestParseSequence(t *testing.T) {
 	}
 }
 
+func TestParseConditional(t *testing.T) {
+	tests := []struct {
+		source     string
+		expected   *Program
+		shouldFail bool
+	}{
+		{
+			"find -name dir | ls || ls /other && grep file",
+			&Program{
+				nodes: []Node{
+					&Conditional{
+						conditionalType: DAND,
+						lhs: &Conditional{
+							conditionalType: DPIPE,
+							lhs: &Pipe{
+								lhs: &SimpleCommand{
+									name:   "find",
+									params: []string{"-name", "dir"},
+								},
+								rhs: &SimpleCommand{
+									name: "ls",
+								},
+							},
+							rhs: &SimpleCommand{
+								name:   "ls",
+								params: []string{"/other"},
+							},
+						},
+						rhs: &SimpleCommand{
+							name:   "grep",
+							params: []string{"file"},
+						},
+					},
+				},
+			},
+			false,
+		},
+		{
+			"grep && awk || sed",
+			&Program{
+				nodes: []Node{
+					&Conditional{
+						conditionalType: DPIPE,
+						lhs: &Conditional{
+							conditionalType: DAND,
+							lhs: &SimpleCommand{
+								name: "grep",
+							},
+							rhs: &SimpleCommand{
+								name: "awk",
+							},
+						},
+						rhs: &SimpleCommand{
+							name: "sed",
+						},
+					},
+				},
+			},
+			false,
+		},
+	}
+
+	for _, tt := range tests {
+		lexer := NewLexer(tt.source)
+		parser := NewParser(lexer)
+		got, err := parser.Parse()
+
+		if tt.shouldFail {
+			if err == nil {
+				t.Errorf(
+					"Expected %s to fail but got %s",
+					tt.source,
+					got,
+				)
+			}
+
+			continue
+		}
+
+		if got.String() != tt.expected.String() {
+			t.Errorf(
+				"Got mismatching ast's for \"%s\": \n got: %s \n expected: %s",
+				tt.source,
+				got,
+				tt.expected,
+			)
+		}
+	}
+}
+
 func TestParseSimpleCommand(t *testing.T) {
 	tests := []struct {
 		source   string
